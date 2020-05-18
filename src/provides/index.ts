dc3b1e49834eaac49c473a508ccd00995dd792b2
MiChen <cmlingyi@yeah.net>
2020-05-18 23:29:59 +0800

    feat: update button

--- src/provides/index.ts
+++ src/provides/index.ts
@@ -1,6 +1,13 @@
-import { InjectionKey } from 'vue'
+import { InjectionKey, inject } from 'vue'
+
+export const ElGlobalConfigSymbol: InjectionKey<{
+    size?: 'medium'|'small'|'mini'
+}> = Symbol()
+
+export function useGlobal(){
+    return inject(ElGlobalConfigSymbol,null)
+}
 
 export const ElRowSymbol: InjectionKey<number> = Symbol()
 export const ElFormSymbol: InjectionKey<any> = Symbol()
-
 export const ElFormItemSymbol: InjectionKey<any> = Symbol()
