439150e2cfda79a5d1862f7c7a750dbc4ef3cd58
jaskang <kangchaomin@dayuwuxian.com>
2020-05-18 18:25:23 +0800

    chore: support eslint-plugin-vue v7

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,21 @@ module.exports = {
   env: {
     node: true
   },
+  parser: 'vue-eslint-parser',
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    sourceType: 'module'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/component-tags-order': 'off'
   }
 }
