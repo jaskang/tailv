79ce33f7665b470584e9efddd4c9eca16e7fa8f3
jaskang <kangchaomin@dayuwuxian.com>
2020-07-04 01:35:54 +0800

    feat: jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    __DEV__: true,
+    __TEST__: true,
+    __VERSION__: require('./package.json').version,
+    __BROWSER__: false,
+    __GLOBAL__: false,
+    __ESM_BUNDLER__: true,
+    __ESM_BROWSER__: false,
+    __NODE_JS__: true,
+    __FEATURE_OPTIONS__: true,
+    __FEATURE_SUSPENSE__: true
+  },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'lcov', 'text'],
+  collectCoverageFrom: ['packages/*/src/**/*.ts'],
+  watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  moduleNameMapper: {},
+  rootDir: __dirname,
+  testMatch: ['<rootDir>/packages/**/__tests__/**/*spec.[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/']
+};
