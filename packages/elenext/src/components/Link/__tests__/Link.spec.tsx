f471ce4455a78c5b1dc2973310823f9effcaf63f
jaskang <kangchaomin@dayuwuxian.com>
2020-10-17 19:32:52 +0800

    tests: fix jest test

--- packages/elenext/src/components/Link/__tests__/Link.spec.tsx
+++ packages/elenext/src/components/Link/__tests__/Link.spec.tsx
@@ -1,78 +1,74 @@
 import { mount } from '@vue/test-utils'
 import { ElLink } from '../'
-import { reactive } from 'vue'
-describe('Link', () => {
-  it('create', () => {
-    const wrapper = mount({
-      render() {
-        return <ElLink type="primary"></ElLink>
-      }
-    })
-    expect(wrapper.classes()).toContain('el-link--primary')
-  })
-  it('icon', () => {
-    const wrapper = mount({
-      render() {
-        return <ElLink icon="el-icon-search"></ElLink>
-      }
-    })
-    expect(wrapper.element.querySelector('.el-icon-search')).toBeDefined()
-  })
-  it('href', () => {
-    const wrapper = mount({
-      render() {
-        return <ElLink href="https://element.eleme.io/"></ElLink>
-      }
-    })
-    expect(wrapper.element.getAttribute('href')).toEqual(
-      'https://element.eleme.io/'
-    )
-  })
-  it('target', () => {
-    const wrapper = mount({
-      render() {
-        return (
-          <ElLink href="https://element.eleme.io/" target="_blank">
-            default
-          </ElLink>
-        )
-      }
-    })
-    expect(wrapper.element.getAttribute('target')).toEqual('_blank')
-  })
-  it('disabled', () => {
-    const wrapper = mount({
-      render() {
-        return <ElLink disabled>default</ElLink>
-      }
-    })
-    expect(wrapper.classes()).toContain('is-disabled')
-  })
 
-  it('click', async () => {
-    let times = 0
-
-    const wrapper = mount({
-      props: {
-        disabled: {
-          type: Boolean
-        }
-      },
-      setup(props) {
-        const handleClick = () => {
-          times++
-        }
-        return () => (
-          <ElLink disabled={props.disabled} onClick={handleClick}>
-            default
-          </ElLink>
-        )
-      }
-    })
-    await wrapper.find('.el-link').trigger('click')
-    expect(times).toBe(1)
-    await wrapper.setProps({ disabled: true })
-    await wrapper.find('.el-link').trigger('click')
-    expect(times).toBe(1)
+it('create', () => {
+  const wrapper = mount({
+    render() {
+      return <ElLink type="primary"></ElLink>
+    }
   })
+  expect(wrapper.classes()).toContain('el-link--primary')
+})
+it('icon', () => {
+  const wrapper = mount({
+    render() {
+      return <ElLink icon="el-icon-search"></ElLink>
+    }
+  })
+  expect(wrapper.element.querySelector('.el-icon-search')).toBeDefined()
+})
+it('href', () => {
+  const wrapper = mount({
+    render() {
+      return <ElLink href="https://element.eleme.io/"></ElLink>
+    }
+  })
+  expect(wrapper.element.getAttribute('href')).toEqual('https://element.eleme.io/')
+})
+it('target', () => {
+  const wrapper = mount({
+    render() {
+      return (
+        <ElLink href="https://element.eleme.io/" target="_blank">
+          default
+        </ElLink>
+      )
+    }
+  })
+  expect(wrapper.element.getAttribute('target')).toEqual('_blank')
+})
+it('disabled', () => {
+  const wrapper = mount({
+    render() {
+      return <ElLink disabled>default</ElLink>
+    }
+  })
+  expect(wrapper.classes()).toContain('is-disabled')
+})
+
+it('click', async () => {
+  let times = 0
+
+  const wrapper = mount({
+    props: {
+      disabled: {
+        type: Boolean
+      }
+    },
+    setup(props) {
+      const handleClick = () => {
+        times++
+      }
+      return () => (
+        <ElLink disabled={props.disabled} onClick={handleClick}>
+          default
+        </ElLink>
+      )
+    }
+  })
+  await wrapper.find('.el-link').trigger('click')
+  expect(times).toBe(1)
+  await wrapper.setProps({ disabled: true })
+  await wrapper.find('.el-link').trigger('click')
+  expect(times).toBe(1)
 })
--- site/src/__docs__/Container.zh-CN.md
+++ site/src/__docs__/Container.zh-CN.md
@@ -53,59 +53,57 @@
 
 ### 实例
 
-:::demo
-
 ```vue
 <template>
   <el-container style="height: 500px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu :default-openeds="['1', '3']">
+      <el-nav :default-openeds="['1', '3']">
         <el-submenu index="1">
           <template slot="title"> <i class="el-icon-message"></i>导航一 </template>
-          <el-menu-item-group>
+          <el-nav-item-group>
             <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
+            <el-nav-item index="1-1">选项1</el-nav-item>
+            <el-nav-item index="1-2">选项2</el-nav-item>
+          </el-nav-item-group>
+          <el-nav-item-group title="分组2">
+            <el-nav-item index="1-3">选项3</el-nav-item>
+          </el-nav-item-group>
           <el-submenu index="1-4">
             <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            <el-nav-item index="1-4-1">选项4-1</el-nav-item>
           </el-submenu>
         </el-submenu>
         <el-submenu index="2">
           <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-          <el-menu-item-group>
+          <el-nav-item-group>
             <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
+            <el-nav-item index="2-1">选项1</el-nav-item>
+            <el-nav-item index="2-2">选项2</el-nav-item>
+          </el-nav-item-group>
+          <el-nav-item-group title="分组2">
+            <el-nav-item index="2-3">选项3</el-nav-item>
+          </el-nav-item-group>
           <el-submenu index="2-4">
             <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            <el-nav-item index="2-4-1">选项4-1</el-nav-item>
           </el-submenu>
         </el-submenu>
         <el-submenu index="3">
           <template slot="title"> <i class="el-icon-setting"></i>导航三 </template>
-          <el-menu-item-group>
+          <el-nav-item-group>
             <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
+            <el-nav-item index="3-1">选项1</el-nav-item>
+            <el-nav-item index="3-2">选项2</el-nav-item>
+          </el-nav-item-group>
+          <el-nav-item-group title="分组2">
+            <el-nav-item index="3-3">选项3</el-nav-item>
+          </el-nav-item-group>
           <el-submenu index="3-4">
             <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            <el-nav-item index="3-4-1">选项4-1</el-nav-item>
           </el-submenu>
         </el-submenu>
-      </el-menu>
+      </el-nav>
     </el-aside>
 
     <el-container>
@@ -171,8 +169,6 @@ export default {
 }
 </script>
 ```
-
-:::
 
 ### Container Attributes
 
