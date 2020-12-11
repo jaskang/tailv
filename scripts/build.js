6784dac823b3b0e296162bf50d0823e5c25fa233
jaskang <kangchaomin@dayuwuxian.com>
2020-12-11 16:15:27 +0800

    build: build all

---
+++ scripts/build.js
@@ -0,0 +1,10 @@
+const execa = require('execa')
+
+async function buildAll(target) {
+  await execa.command('yarn workspace @elenext/shared build', { stdio: 'inherit' })
+  await execa.command('yarn workspace @elenext/icons build', { stdio: 'inherit' })
+  await execa.command('yarn workspace elenext build', { stdio: 'inherit' })
+  await execa.command('yarn workspace @elenext/docs build', { stdio: 'inherit' })
+}
+
+buildAll()
