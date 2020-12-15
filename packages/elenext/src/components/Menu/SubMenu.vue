5745dd24789688063526b2da9ea3e9123713428c
jaskang <kangchaomin@dayuwuxian.com>
2020-12-15 19:36:50 +0800

    fix(menu): menu popper trigger hover

--- packages/elenext/src/components/Menu/SubMenu.vue
+++ packages/elenext/src/components/Menu/SubMenu.vue
@@ -20,7 +20,7 @@
       <Popper
         popper-class="el-sub-menu"
         placement="right-start"
-        trigger="click"
+        trigger="hover"
         mode="outer"
         :arrow="false"
         :reference="titleElRef"
