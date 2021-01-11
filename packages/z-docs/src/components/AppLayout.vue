60511e843912e753d88413234447d1de4e6b4533
jaskang <jaskang@qq.com>
2021-01-11 23:18:49 +0800

    docs: layout center

--- packages/z-docs/src/components/AppLayout.vue
+++ packages/z-docs/src/components/AppLayout.vue
@@ -2,44 +2,58 @@
   <div class="demo-layout">
     <e-layout>
       <e-header class="demo-header">
-        <e-row align="middle">
-          <e-col flex="1">
-            <Logo />
+        <e-row>
+          <e-col :flex="screens.xl ? 1 : 0"></e-col>
+          <e-col :flex="screens.xl ? '1200px' : 1">
+            <e-row align="middle">
+              <e-col :flex="1">
+                <Logo />
+              </e-col>
+              <e-col>
+                <e-menu mode="horizontal">
+                  <e-menu-item>
+                    <a href="https://github.com/JasKang/elenext" target="__blank">GitHub</a>
+                  </e-menu-item>
+                </e-menu>
+              </e-col>
+            </e-row>
           </e-col>
-          <e-col>
-            <e-menu mode="horizontal">
-              <e-menu-item><i class="el-icon-location" />导航一</e-menu-item>
-              <e-menu-item><i class="el-icon-document" />导航三</e-menu-item>
-              <e-menu-item>
-                <a href="https://github.com/JasKang/elenext" target="__blank">GitHub</a>
-              </e-menu-item>
-            </e-menu>
-          </e-col>
+          <e-col :flex="screens.xl ? 1 : 0"></e-col>
         </e-row>
       </e-header>
-      <e-layout>
-        <e-aside class="demo-aside">
-          <div style="padding-top: 40px" />
-          <e-menu mode="vertical" :current-path="route.path">
-            <template v-for="menu in menus" :key="menu.title">
-              <e-menu-itemGroup :title="menu.title">
-                <e-menu-item v-for="item in menu.items" :key="item" :path="`/${item}`">
-                  {{ item }}
-                </e-menu-item>
-              </e-menu-itemGroup>
-            </template>
-          </e-menu>
-        </e-aside>
-        <e-main>
-          <div class="site-content">
-            <router-view />
-          </div>
-        </e-main>
-      </e-layout>
+      <e-main>
+        <e-row>
+          <e-col :flex="screens.xl ? 1 : 0"></e-col>
+          <e-col :flex="screens.xl ? '1200px' : 1">
+            <e-row align="top" :wrap="false">
+              <e-col :flex="'200px'">
+                <div style="padding-top: 40px">
+                  <e-menu mode="vertical" :current-path="route.path">
+                    <template v-for="menu in menus" :key="menu.title">
+                      <e-menu-itemGroup :title="menu.title">
+                        <e-menu-item v-for="item in menu.items" :key="item" :path="`/${item}`">
+                          {{ item }}
+                        </e-menu-item>
+                      </e-menu-itemGroup>
+                    </template>
+                  </e-menu>
+                </div>
+              </e-col>
+              <e-col :flex="1">
+                <div class="site-content">
+                  <router-view />
+                </div>
+              </e-col>
+            </e-row>
+          </e-col>
+          <e-col :flex="screens.xl ? 1 : 0"></e-col>
+        </e-row>
+      </e-main>
     </e-layout>
   </div>
 </template>
 <script lang="ts">
+import { useBreakpoint } from 'elenext'
 import { defineComponent, watchEffect } from 'vue'
 import { useRouter, useRoute, RouteLocationRaw } from 'vue-router'
 import menus from '../menus'
@@ -52,14 +66,12 @@ export default defineComponent({
   setup() {
     const router = useRouter()
     const route = useRoute()
+    const screens = useBreakpoint()
     watchEffect(() => {
       console.log(route.path)
     })
-    const push = (e: RouteLocationRaw) => {
-      router.push(e)
-    }
     return {
-      push,
+      screens,
       route,
       menus
     }
@@ -70,33 +82,13 @@ export default defineComponent({
 .demo-layout {
   height: 100vh;
 }
-.demo-content {
-  height: 100vh;
-  overflow-y: auto;
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    background-color: #fff;
-    font-size: 14px;
-    margin-bottom: 45px;
-    line-height: 1.5em;
-    th {
-      text-align: left;
-      white-space: nowrap;
-      color: #909399;
-      font-weight: 400;
-    }
-    td,
-    th {
-      border-bottom: 1px solid #dcdfe6;
-      padding: 15px;
-      max-width: 250px;
-      &:first-child {
-        padding-left: 10px;
-      }
-    }
-  }
+.site-content {
+  width: 100%;
+  padding: 20px;
+  // max-width: 900px;
+  margin: 0 auto;
 }
+
 .demo-aside {
   border-right: solid 1px #e6e6e6;
 }
