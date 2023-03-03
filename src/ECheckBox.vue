38eb44f82d0f13d534d36fa87f1a9e4140d32f22
JasKang <jaskang@qq.com>
2023-03-03 08:58:00 +0800

    feat: checkbox

--- src/ECheckBox.vue
+++ src/ECheckBox.vue
@@ -14,6 +14,8 @@ const innerChecked = ref(props.checked)
 
 const onInput = (e: Event) => {
   const el = e.currentTarget as HTMLInputElement
+  console.log(el.checked)
+
   innerChecked.value = el.checked
 }
 const onClick = (e: Event) => {
@@ -25,16 +27,30 @@ const onClick = (e: Event) => {
 </script>
 <template>
   <label :class="['e-checkbox', disabled && 'is-disabled']">
-    <input
-      type="checkbox"
-      class="e-checkbox__input font-extrabold"
-      :value="value"
-      :name="name"
-      :disabled="disabled"
-      :checked="innerChecked"
-      @input="onInput"
-      @click="onClick"
-    />
+    <span class="e-checkbox__input">
+      <input
+        type="checkbox"
+        class="e-checkbox__original"
+        :value="value"
+        :name="name"
+        :disabled="disabled"
+        :checked="innerChecked"
+        @input="onInput"
+        @click="onClick"
+      />
+      <span class="e-checkbox__inner">
+        <svg
+          v-show="innerChecked"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z"
+          />
+        </svg>
+      </span>
+    </span>
     <span class="e-checkbox__label"><slot /></span>
   </label>
 </template>
