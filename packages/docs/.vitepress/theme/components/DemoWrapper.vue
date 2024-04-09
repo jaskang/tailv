3bccd9665564654e71abfe8930ddce61ff2772c7
Jaskang <jaskang@qq.com>
2024-04-09 15:06:03 +0800

    feat: demo bg

--- packages/docs/.vitepress/theme/components/DemoWrapper.vue
+++ packages/docs/.vitepress/theme/components/DemoWrapper.vue
@@ -31,7 +31,7 @@ const showCode = ref(false)
       '--tw-prose-pre-bg': 'transparent',
     }"
   >
-    <div class="flex items-center justify-end bg-slate-50 py-1 px-2">
+    <div class="demo-wrapper-svg-bg flex items-center justify-end bg-slate-50 py-1 px-2">
       <div class="flex-1">
         <svg viewBox="0 0 32 32" class="h-4 w-4">
           <path fill="#41b883" d="M24.4 3.925H30l-14 24.15L2 3.925h10.71l3.29 5.6l3.22-5.6Z"></path>
@@ -46,7 +46,7 @@ const showCode = ref(false)
         </Switch>
       </div>
     </div>
-    <div class="not-prose demo-wrapper-svg-bg bg-white p-4 dark:bg-slate-900">
+    <div class="not-prose bg-white p-4 dark:bg-slate-900">
       <slot />
     </div>
 
@@ -59,7 +59,7 @@ const showCode = ref(false)
 .demo-wrapper-svg-bg {
   background-position: 8px 8px;
   background-size: 16px 16px;
-  background-color: var(--color-white);
+  background-color: var(--color-slate-50);
   /* slate-100 */
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e");
 }
