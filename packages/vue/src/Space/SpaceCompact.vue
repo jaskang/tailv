3d8b92a9416a1fcb8f7018f3aefbca57c7b08d06
Jaskang <jaskang@qq.com>
2024-04-10 18:07:14 +0800

    feat: spacecompact

--- packages/vue/src/Space/SpaceCompact.vue
+++ packages/vue/src/Space/SpaceCompact.vue
@@ -5,7 +5,9 @@ const slots = defineSlots<{ default?(_: {}): any }>()
 const props = defineProps({})
 </script>
 <template>
-  <div class="t-space-compact flex items-center *:-me-[1px] *:rounded-none first:*:rounded-l-md last:*:rounded-r-md">
+  <div
+    class="t-space-compact flex items-center *:-me-[1px] *:rounded-none [&>:last-child]:rounded-r-md [&>:first-child]:rounded-l-md"
+  >
     <slot />
   </div>
 </template>
