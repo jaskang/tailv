7571dd26a06a8e98f31b32dbc320a4a0f5844ca5
Jaskang <jaskang@qq.com>
2024-04-28 10:37:10 +0800

    feat: button

--- src/Button/index.vue
+++ src/Button/index.vue
@@ -31,18 +31,26 @@ const props = defineProps({
   <button
     type="button"
     :style="{
+      '--accent-50': cvar(`${props.color}-50`),
       '--accent-100': cvar(`${props.color}-100`),
       '--accent-200': cvar(`${props.color}-200`),
+      '--accent-300': cvar(`${props.color}-300`),
+      '--accent-400': cvar(`${props.color}-400`),
       '--accent-500': cvar(`${props.color}-500`),
       '--accent-600': cvar(`${props.color}-600`),
+      '--accent-800': cvar(`${props.color}-800`),
+      '--accent-900': cvar(`${props.color}-900`),
+      '--accent-950': cvar(`${props.color}-950`),
     }"
     class="inline-flex items-center justify-center whitespace-nowrap text-center font-medium ring-offset-white transition-colors focus-visible:z-10 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
     :class="[
       {
         solid: 'bg-[--accent-500] text-white hover:bg-[--accent-600]',
-        soft: 'bg-[--accent-100] text-[--accent-500] hover:bg-[--accent-200] hover:text-[--accent-600]',
-        ghost: 'text-[--accent-500] hover:bg-[--accent-100] hover:text-[--accent-600]',
-        outline: 'border border-gray-200 bg-white hover:bg-gray-100',
+        soft: 'bg-[--accent-100] text-[--accent-500] hover:bg-[--accent-200] hover:text-[--accent-600] dark:bg-[--accent-900] dark:text-[--accent-400] dark:hover:bg-[--accent-800] dark:hover:text-[--accent-300]',
+        ghost:
+          'text-[--accent-500] hover:bg-[--accent-100] hover:text-[--accent-600] dark:text-[--accent-400] dark:hover:bg-[--accent-900] dark:hover:text-[--accent-300]',
+        outline:
+          'border border-gray-200 bg-white hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800',
         link: 'underline-offset-4 hover:underline',
       }[props.variant],
       {
