17cdc259baf35e7d6ae549e4edb4f4bac36b45c3
Jaskang <jaskang@qq.com>
2024-05-24 17:43:45 +0800

    feat: select

--- site/components/select.md
+++ site/components/select.md
@@ -5,7 +5,7 @@
 ```vue demo
 <template>
   <div class="flex flex-wrap gap-2">
-    <Select
+    <TSelect
       class="w-60"
       placeholder="选择框"
       :options="[
@@ -21,7 +21,7 @@
         { value: 10, label: 'Emil Schaefer' },
       ]"
     />
-    <Select
+    <TSelect
       class="w-60"
       placeholder="选择框"
       :options="[
@@ -39,7 +39,7 @@
 ```vue demo
 <template>
   <div class="flex flex-wrap gap-2">
-    <Select
+    <TSelect
       class="w-60"
       placeholder="选择框"
       value="2"
@@ -50,6 +50,34 @@
         { value: '3', label: 'Devon Webb' },
       ]"
     />
+  </div>
+</template>
+```
+
+## template
+
+```vue demo
+<template>
+  <div class="flex flex-wrap gap-2">
+    <TSelect
+      class="w-60"
+      placeholder="选择框"
+      value="2"
+      :options="[
+        { value: '1', label: 'Wade Cooper', src: 'https://i.pravatar.cc/150?u=1' },
+        { value: '2', label: 'Arlene Mccoy', src: 'https://i.pravatar.cc/150?u=2' },
+        { value: '3', label: 'Devon Webb', src: 'https://i.pravatar.cc/150?u=3' },
+      ]"
+    >
+      <template #item="{ item }">
+        <div class="flex flex-1 items-center gap-1 p-1">
+          <Avatar rounded border size="sm" :src="item.src" />
+          <span>
+            {{ item.label }}
+          </span>
+        </div>
+      </template>
+    </TSelect>
   </div>
 </template>
 ```
--- src/Select/Select.vue
+++ src/Select/Select.vue
@@ -12,6 +12,7 @@ import { useModelValue } from '../use/useModelValue'
 defineOptions({ name: 'TSelect', inheritAttrs: false })
 
 const emit = defineEmits(['update:value', 'change', 'select'])
+const slots = defineSlots<{ item?: (props: { active: boolean; item?: SelectOption; placeholder?: string }) => any }>()
 const props = defineProps({
   value: [String, Number],
   options: {
@@ -30,10 +31,9 @@ const [modelValue, setModelValue] = useModelValue(props, {
   },
 })
 
-const label = computed(() => {
-  const item = props.options.find(item => item.value === modelValue.value)
-  return item ? item.label : ''
-})
+const currItem = computed(() => props.options.find(item => item.value === modelValue.value))
+
+const label = computed(() => currItem.value?.label || '')
 
 const popoverRef = ref<InstanceType<typeof Popover>>()
 const inputRef = ref<InstanceType<typeof Input>>()
@@ -50,17 +50,18 @@ const focused = ref(false)
 <template>
   <Popover trigger="click" placement="bottom-start" ref="popoverRef" size-mode="min-width" @change="v => (focused = v)">
     <button
-      class="flex h-9 items-center justify-between rounded-md border border-border bg-transparent px-3 text-sm leading-[1.375rem] shadow-sm focus-visible:z-10 focus-visible:border-primary focus-visible:ring-1 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50"
+      class="flex h-9 items-center rounded-md border border-border bg-transparent px-1 text-left text-sm shadow-sm focus-visible:z-10 focus-visible:border-primary focus-visible:ring-1 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50"
       v-bind="$attrs"
       :disabled
     >
-      <span
-        class="pointer-events-none overflow-hidden text-ellipsis whitespace-nowrap"
-        :class="[label ? '' : 'text-default-400']"
-      >
-        {{ label || placeholder }}
-      </span>
-      <ChevronDownIcon class="-mr-1 ml-1 h-4 w-4 flex-shrink-0"></ChevronDownIcon>
+      <div class="flex w-full flex-1 items-center" :class="[slots.item ? '' : 'p-2']">
+        <slot name="item" :active="false" :item="currItem" :placeholder>
+          <span class="flex-1 overflow-hidden text-ellipsis text-nowrap" :class="[label ? '' : 'text-default-400']">
+            {{ label || placeholder }}
+          </span>
+        </slot>
+      </div>
+      <ChevronDownIcon class="mx-1 h-4 w-4 shrink-0"></ChevronDownIcon>
     </button>
     <template #content>
       <ScrollArea class="flex max-h-80 flex-col rounded text-sm shadow-md ring-1 ring-border" mode="y">
@@ -68,15 +69,17 @@ const focused = ref(false)
           <template #item="{ item }">
             <div
               @click="selectHandler(item)"
-              class="flex items-center p-2"
-              :class="[item.value === modelValue ? 'bg-primary font-medium text-white' : '']"
+              class="flex items-center"
+              :class="[slots.item ? '' : 'p-2', item.value === modelValue ? 'font-medium' : '']"
             >
-              <span class="flex-1 overflow-hidden text-ellipsis text-nowrap">
-                {{ item.label }}
-              </span>
-              <!-- <span v-if="item.value === modelValue" class="shrink-0 pl-1">
-                <CheckIcon class="h-4 w-4" />
-              </span> -->
+              <slot name="item" :active="item.value === modelValue" :item="item">
+                <span class="flex-1 overflow-hidden text-ellipsis text-nowrap">
+                  {{ item.label }}
+                </span>
+                <span v-if="item.value === modelValue" class="shrink-0 pl-1">
+                  <CheckIcon class="h-4 w-4" />
+                </span>
+              </slot>
             </div>
           </template>
         </ListBox>
